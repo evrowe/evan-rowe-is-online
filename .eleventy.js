88cc7910f147497e21d7d6a3b8a4d5af61b10e91
Glitch (first-end) <none>
2022-04-19 18:35:10 +0000

    👰🙆 Checkpoint ./src/index.html:998504/317 ./src/_includes/layouts/index.njk:998504/108 ./src/_includes/layouts/posts.njk:998504/491 ./.eleventy.js:998504/197 ./src/posts/test.md:998504/229 ./src/_includes/layouts/post.njk:998504/937 ./src/posts/posts.json:998504/24 ./src/_includes/layouts/base.njk:998504/126

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,20 @@
 module.exports = function(eleventyConfig) {
   
   // eleventyConfig.addFilter( "myFilter", function() {});
+  eleventyConfig.setTemplateFormats([
+    // Templates:
+    "html",
+    "njk",
+    "md",
+    // Static Assets:
+    "css",
+    "jpeg",
+    "jpg",
+    "png",
+    "svg",
+    "woff",
+    "woff2"
+  ]);
   
   eleventyConfig.addPassthroughCopy("public");
   
---
+++ src/posts/test.md
@@ -0,0 +1,8 @@
+---
+title: This is a sample post
+description: Add posts and your site build will publish them
+date: 2022-04-19
+tags:
+  - posts
+layout: layouts/post.njk
+---
