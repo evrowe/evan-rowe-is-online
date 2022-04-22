24e0b72f29b0363f7566f32fca5aa1d9a5ca9d0c
Glitch (first-end) <none>
2022-04-22 22:55:04 +0000

    📷🐯 Checkpoint ./public/hacking.js:998504/1688 ./src/_includes/layouts/base.njk:998504/1502

--- public/hacking.js
+++ public/hacking.js
@@ -1,22 +1,9 @@
-// Delegate events
-const on = (selector, eventType, childSelector, eventHandler) => {
-  const elements = document.querySelectorAll(selector)
-  
-  elements.forEach( element => {
-    element.addEventListener(eventType, eventOnElement => {
-      if (eventOnElement.target.matches(childSelector)) {
-        eventHandler(eventOnElement)
-      }
-    })
-  })
-}
-
 // check for current system-level theme
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 // Listen for clicks on the theme switcher
-on('.toggler', 'click', '.slider', e => {
-  console.log('hi')
+const toggleTheme = e => {
+  if (e.target.className !== 'slider') return
   let body = document.querySelector('body')
 
   if (body.hasAttribute('data-theme') && body.attributes['data-theme'] == 'dark') {
@@ -24,4 +11,6 @@ on('.toggler', 'click', '.slider', e => {
   } else {
     prefersDarkMode ? body.setAttribute('data-theme', 'dark') : body.removeAttribute('data-theme')
   }
-})
+}
+
+document.addEventListener('click', toggleTheme)
