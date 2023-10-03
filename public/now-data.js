8bc5e35a14f4f187f990fb7e41c4eedbd6295f37
Glitch (first-end) <none>
2023-10-03 04:09:06 +0000

    🐴🍿 Checkpoint ./public/now-data.js:998504/822

--- public/now-data.js
+++ public/now-data.js
@@ -2,15 +2,15 @@ const OPENCRITIC_API_BASE_URL = 'opencritic-api.p.rapidapi.com'
 const OPENCRITIC_API_KEY = 'fe3f31aacfmsh87285e4f8848e25p1724b4jsn22c2d47e2056'
 
 const nowData = {
-  "listening-comment": "",
-  "listening": [],
-  "playing-comment": "a list of OpenCritic game IDs, to populate data from the OpenCritic API at opencritic-api.p.rapidapi.com",
-  "playing": [8525,15580,15003,14907,15151,15125,9136,1887],
-  "watching-comment": "",
-  "watching": []
+  'listening-comment': '',
+  'listening': [],
+  'playing-comment': 'a list of OpenCritic game IDs, to populate data from the OpenCritic API at opencritic-api.p.rapidapi.com',
+  'playing': [8525,15580,15003,14907,15151,15125,9136,1887],
+  'watching-comment': '',
+  'watching': []
 }
 
-async function fetchGame(gameID) {
+async function fetchGameData(gameID) {
   const options = {
 	  method: 'GET',
 	  headers: {
@@ -23,3 +23,20 @@ async function fetchGame(gameID) {
   return game
 }
 
+async function fetchGamesData(gameIDs = []) {
+  if (!(gameIDs instanceof Array) || !gameIDs.length) return
+  return await gameIDs.map(async function(id, index) {
+    const game = await fetchGameData(id)
+    return {
+      id,
+      image: game.images.box.sm,
+      name: game.name,
+      developer: game.companies[0].name
+    }
+  })
+}
+
+async function buildGamesHTML () {
+  await fetchGamesData(nowData.playing)
+  if 
+}
