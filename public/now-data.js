199cfd5f15f3978f927e41bd17362d7e8273bc77
Glitch (first-end) <none>
2023-10-03 03:57:38 +0000

    🐙🐀 Checkpoint ./public/now-data.js:998504/1358

--- public/now-data.js
+++ public/now-data.js
@@ -1,9 +1,25 @@
-const OPENCRITIC_API_BASE_URL = 'https://opencritic-api.p.rapidapi.com/'
+const OPENCRITIC_API_BASE_URL = 'opencritic-api.p.rapidapi.com'
+const OPENCRITIC_API_KEY = 'fe3f31aacfmsh87285e4f8848e25p1724b4jsn22c2d47e2056'
 
+const nowData = {
+  "listening-comment": "",
+  "listening": [],
+  "playing-comment": "a list of OpenCritic game IDs, to populate data from the OpenCritic API at opencritic-api.p.rapidapi.com",
+  "playing": [8525,15580,15003,14907,15151,15125,9136,1887],
+  "watching-comment": "",
+  "watching": []
+}
 
-async function fetchGames() {
-  const response = await fetch(`${OPENCRITIC_API_BASE_URL}game/`)
-  const games = await response.json()
-  return games 
+async function fetchGame(gameID) {
+  const options = {
+	  method: 'GET',
+	  headers: {
+		  'X-RapidAPI-Key': OPENCRITIC_API_KEY,
+		  'X-RapidAPI-Host': OPENCRITIC_API_BASE_URL
+	  }
+  };
+  const response = await fetch(`https://${OPENCRITIC_API_BASE_URL}/game/${gameID}`, options)
+  const game = await response.json()
+  return game
 }
 
