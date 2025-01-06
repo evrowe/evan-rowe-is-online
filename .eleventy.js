40655170fce056754e56a77739a1253ee64b1ff5
Glitch (evan-rowe-is-online) <none>
2025-01-06 22:00:42 +0000

    🍚💶 Checkpoint ./public/style.css:998504/170 ./src/_includes/layouts/post.njk:998504/155 ./.eleventy.js:998504/189

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
+  
+  eleventyConfig.addFilter("htmlNiceDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("MMM d, yyyy")
+  })
 
   eleventyConfig.setServerOptions({
     // Defaults
